ecc8fce2451e89a929f07bc1c10ec38e82f9a445
Evan Oman <evan058@gmail.com>
2016-09-19 23:04:49 -0500

    Working on join :rocket:

--- hacking.R
+++ hacking.R
@@ -1,38 +1,50 @@
 library("reshape2")
 library("plyr")
 
-rm(list=ls())
+# Writes off the data to csv
+write.csv(processDraftData(), "./reorderedDraft.csv", row.names = FALSE)
 
-draftData <- read.csv("./Draft.csv")
+# Read in draft data
+draftData <- read.csv("./reorderedDraft.csv")
 
-draftData <- melt(draftData, id.vars = "ROUND")
+# read in ESPN Data
+espn <- read.csv("./ESPN_Rankings.csv")
 
-draftData <- draftData[order(draftData$ROUND), ]
+processDraftData <- function()
+{
+  
+  draftData <- read.csv("./Draft.csv")
+  
+  draftData <- melt(draftData, id.vars = "ROUND")
+  
+  draftData <- draftData[order(draftData$ROUND), ]
+  
+  draftData$pickNum <- lapply(1:(16*8), snakeFunction)
+  
+  draftData <- as.data.frame(lapply(draftData, unlist))
+  
+  draftData <- draftData[order(draftData$pickNum),]
+  
+  rename(draftData, c("variable" = "Drafter", "value" = "Player", "pickNum" = "Pick #", "ROUND" = "Round"))
+}
 
 # Creates the sequence: 1,2,3,4,5,6,7,8,16,15,14,13,12,11,10,9,17,18,19,20....
 snakeFunction <- function(i)
 {
-    groupIndicator <- ceiling(i / 8)
-    # Increasing group
-    if (groupIndicator %% 2 == 1)
-    {
-        i
-    }
-    # Decreasing group
-    else
-    {
-        8*groupIndicator - ((i - 1) %% 8)
-    }
+  groupIndicator <- ceiling(i / 8)
+  # Increasing group
+  if (groupIndicator %% 2 == 1)
+  {
+    i
+  }
+  # Decreasing group
+  else
+  {
+    8*groupIndicator - ((i - 1) %% 8)
+  }
 }
 
-snakeIndices <- lapply(1:(16*8), snakeFunction)
 
-draftData$pickNum <- snakeIndices
 
-draftData <- as.data.frame(lapply(draftData, unlist))
 
-draftData <- draftData[order(draftData$pickNum),]
 
-draftData <- rename(draftData, c("variable" = "Drafter", "value" = "Player", "pickNum" = "Pick #", "Round = ROUND"))
-
-# write.csv2(df5, "./reorderedDraft.csv")
